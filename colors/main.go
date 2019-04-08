4eaa5de5cae7085034a62475ab6bc29af6454107
Priya Wadhwa <priyawadhwa@google.com>
2019-04-08 14:20:35 -0700

    making progress

---
+++ colors/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Print("Listening...")
+	http.HandleFunc("/", colors)
+	http.ListenAndServe(":8888", nil)
+}
+
+type Response struct {
+	Colors Colors `json:"colors,omitempty"`
+}
+
+type Colors struct {
+	X string `json:"x,omitempty"`
+	O string `json:"o,omitempty"`
+}
+
+func colors(w http.ResponseWriter, r *http.Request) {
+	resp := &Response{
+		Colors: Colors{
+			X: "black",
+			O: "black",
+		},
+	}
+	contents, err := json.Marshal(resp)
+	if err != nil {
+		log.Printf("error marshaling json: %v", err)
+		return
+	}
+	w.Write(contents)
+}
